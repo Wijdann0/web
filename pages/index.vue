0585abfecdaa0773346db0c0f3bd5b01ee6a670e
Wijdann0 <160085539+Wijdann0@users.noreply.github.com>
2024-05-15 12:33:34 +0700

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,14 @@
 <template>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    
+
   <div class="container-fluid">
     <div class="row my-5 d-flex justify-content-between">
       <div class="col-lg-5 mb-5">
@@ -37,11 +47,14 @@
       </div>
     </div>
   </div>
+</body>
+</html>
 </template>
 
 <script setup>
 const supabase = useSupabaseClient()
 const visitors = ref ([])
+
 
 
 const getPengunjung = async () => {
@@ -52,6 +65,7 @@ const getPengunjung = async () => {
 onMounted(() => {
   getPengunjung()
 })
+
 </script>
 
 <style scoped>
